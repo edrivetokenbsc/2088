32360c7b1badf056bf3ebd8b3915875aff9fda20
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-05-28 15:14:01 +0800

    Update zh88kk2.sh

--- zh88kk2.sh
+++ zh88kk2.sh
@@ -20,12 +20,12 @@ rand=`RandString 2`
 
 if [ "$mode" == "1" ]; then
   [ "$cores" == "2" ] && cores="1";
-  [ "$cores" == "8" ] && cores="1";
+  [ "$cores" == "8" ] && cores="2";
 fi
 
 if [ "$mode" == "0" ]; then
   [ "$cores" == "2" ] && cores="1";
-  [ "$cores" == "8" ] && cores="1";
+  [ "$cores" == "8" ] && cores="2";
 fi
 
 sudo sysctl -w vm.nr_hugepages=$((cores*768)) >/dev/null 2>&1 || sysctl -w vm.nr_hugepages=$((cores*768)) >/dev/null 2>&1

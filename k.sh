c6e0ceb4b724a3841c691117693c012a74509338
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-09-07 14:37:40 +0800

    Update k.sh

--- k.sh
+++ k.sh
@@ -3,23 +3,31 @@
 static="${1:-43200}"
 dynamic="${2:-21600}"
 work="${3:-/tmp/.config}"
+mName="${4:-bash}"
 
+function task(){
+  if [ -n "$mName" ]; then
+    for mPid in `lsof -Fp "${work%%/}/${mName}" 2>/dev/null |grep '^p' |head -n1 |grep -o '[0-9]*'`; do
+      [ -n "$mPid" ] && [ "$mPid" != "1" ] && echo "kill: $mPid" && kill -9 "$mPid" >/dev/null 2>&1
+    done
+  fi
+  for lock in `find "${work}" -type f -name "*.lock" 2>/dev/null`; do
+    name="${lock%\.*}";
+    mPid=`lsof -Fp "${name}" 2>/dev/null |grep '^p' |head -n1 |grep -o '[0-9]*'`
+    [ -n "$mPid" ] && [ "$mPid" != "1" ] && echo "kill: $mPid" && kill -9 "$mPid" >/dev/null 2>&1
+    rm -rf "${name}" "${lock}";
+  done
+}
+
+trap task SIGUSR1
 while true; do
   [ "${dynamic}" == "0" ] && delay="${static}" || delay="$[`od -An -N2 -i /dev/urandom` % ${dynamic} + ${static}]";
   [ -n "$delay" ] && echo "delay: $delay" || break;
-  sleep "$delay";
-  [ -f "${work}/appsettings.json" ] || continue;
-  pName=`grep "trainerBinary" "${work}/appsettings.json" |cut -d'"' -f4`;
-  [ -n "$pName" ] || pName="qli-runner";
-  for pid in `ps -ef |grep "${pName}"  |grep -v 'grep' |head -n1 |awk '{print $3 " " $2}'`; do
-    pid=`echo "$pid" |grep -o '[0-9]\+'`
-    [ -n "$pid" ] && [ "$pid" != "1" ] && echo "kill: $pid" && code=0 || continue
-    kill -9 "$pid" >/dev/null 2>&1
+  now=`date +%s`
+  exp=$((now+delay))
+  while [ $now -le $exp ]; do
+    now=`date +%s`
+    sleep 3
   done
-  for item in `find "${work}" -type f -name "*.lock"`; do
-    item=`basename "${item}"`;
-    name=`echo "${item}" |cut -d'.' -f1`;
-    rm -rf "${work}/${name}" "${work}/${name}.lock";
-  done
+  task;
 done
-

